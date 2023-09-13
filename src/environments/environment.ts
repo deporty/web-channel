0aab06b243d6b221b7e5f3c1b6c78d843070f406
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-09-13 14:38:21 -0500

    Upload all files just at the end of the day. This is a general message.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   analytics: false,
-  serverEndpoint: 'http://localhost:10000',
+  serverEndpoint: 'http://34.107.159.12',
   firebaseConfig: {
     apiKey: "AIzaSyBTy7TeYPAkELnMSrZ7QNm27qvIH62Q4zI",
     authDomain: "deporty-dev.firebaseapp.com",
