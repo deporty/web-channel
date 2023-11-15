9e5dc9ab843580dd4e478d98eb8b26cc4745263c
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-11-15 00:54:47 -0500

    Upload all files just at the end of the day. This is a general message.

--- src/app/features/tournaments/presentation/components/tournament-card/tournament-card.component.ts
+++ src/app/features/tournaments/presentation/components/tournament-card/tournament-card.component.ts
@@ -1,8 +1,10 @@
 import {
+  ChangeDetectorRef,
   Component,
   EventEmitter,
   Input,
   OnChanges,
+  OnInit,
   Output,
   SimpleChanges,
 } from '@angular/core';
@@ -17,27 +19,63 @@ import {
   MatBottomSheet,
   MatBottomSheetRef,
 } from '@angular/material/bottom-sheet';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-tournament-card',
   templateUrl: './tournament-card.component.html',
   styleUrls: ['./tournament-card.component.scss'],
 })
-export class TournamentCardComponent implements OnChanges {
+export class TournamentCardComponent implements OnChanges, OnInit {
   @Input() tournament!: TournamentEntity;
   @Input('tournament-layout') tournamentLayout!: TournamentLayoutEntity;
   @Input('mobile-compatible') mobileCompatible = false;
+  isSmall = false;
   @Output('on-click') onClick: EventEmitter<TournamentEntity>;
 
   img = DEFAULT_TOURNAMENT_LAYOUT_IMG;
   usingTournament = false;
-  constructor(private _bottomSheet: MatBottomSheet) {
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private _bottomSheet: MatBottomSheet,
+    private cdr: ChangeDetectorRef
+  ) {
     this.onClick = new EventEmitter();
+  }
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .subscribe((state) => {
+        if (state.breakpoints[Breakpoints.XSmall]) {
+          console.log('Estás en el breakpoint XSmall');
+          this.isSmall = true;
+        } else if (state.breakpoints[Breakpoints.Small]) {
+          this.isSmall = true;
+          console.log('Estás en el breakpoint Small');
+        } else if (state.breakpoints[Breakpoints.Medium]) {
+          this.isSmall = false;
+          console.log('Estás en el breakpoint Medium');
+        } else if (state.breakpoints[Breakpoints.Large]) {
+          this.isSmall = false;
+          console.log('Estás en el breakpoint Large');
+        } else if (state.breakpoints[Breakpoints.XLarge]) {
+          this.isSmall = false;
+          console.log('Estás en el breakpoint XLarge');
+        }
+        console.log(this.isSmall);
+        this.cdr.detectChanges();
+      });
   }
 
   openBottomSheet(): void {
     this._bottomSheet.open(TournamentCardDetailsComponent, {
-
       panelClass: 'bottom-sheet-container-with-no-padding',
       data: {
         tournament: this.tournament,
