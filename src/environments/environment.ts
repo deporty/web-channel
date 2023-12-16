9caeebca9e0c621ea76a2180e380268d15b3b46d
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-12-15 22:27:59 -0500

    Upload all files just at the end of the day. This is a general message.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
-  analytics: false,
-  serverEndpoint: 'http://127.0.0.1:10000',
+  production: true,
+  analytics: true,
+  serverEndpoint: 'https://api.deporty.com',
   firebaseConfig: {
-    apiKey: "AIzaSyBTy7TeYPAkELnMSrZ7QNm27qvIH62Q4zI",
-    authDomain: "deporty-dev.firebaseapp.com",
-    projectId: "deporty-dev",
-    storageBucket: "deporty-dev.appspot.com",
-    messagingSenderId: "186958009196",
-    appId: "1:186958009196:web:05533df8a1c7a7e3251949",
-    measurementId: "G-4KH8KW18GH"
+    apiKey: "AIzaSyCmfXDNcDTHtWgc8DnieEk0MS6KUogR-Rc",
+    authDomain: "deporty-app.firebaseapp.com",
+    projectId: "deporty-app",
+    storageBucket: "deporty-app.appspot.com",
+    messagingSenderId: "861456172435",
+    appId: "1:861456172435:web:edaf5251b6866e8fe8d466",
+    measurementId: "G-NLEM82Z201"
   }
 };
-
-/*
- * For easier debugging in appelopment mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
